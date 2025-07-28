dfda1cbea9533c8e8803e804545edd01fd44aefe
sonya02022010 <sofya.timonina@internet.ru>
2025-07-28 18:52:27 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,30 @@
-meme_dict = {
-            "КРИНЖ": "Что-то очень странное или стыдное",
-            "ЛОЛ": "Что-то очень смешное"
-            }
-word = input("Введите непонятное слово (большими буквами!): ")
-if word in meme_dict.keys():
-     print(meme_dict[word])
-else:
-    print("we are have no this word")
+ import telebot
+    
+    # Замени 'TOKEN' на токен твоего бота
+    # Этот токен ты получаешь от BotFather, чтобы бот мог работать
+bot = telebot.TeleBot("8023355520:AAGolIoAJZJC8vdDsXaI37IlomUAUL8tdK4")
+    
+@bot.message_handler(commands=['start'])
+def send_welcome(message):
+    bot.reply_to(message, "Привет! Я твой Telegram бот. Напиши что-нибудь!")
+    
+@bot.message_handler(commands=['hello'])
+def send_hello(message):
+    bot.reply_to(message, "Привет! Как дела?")
+    
+@bot.message_handler(commands=['bye'])
+def send_bye(message):
+    bot.reply_to(message, "Пока! Удачи!")
+    
+    
+ 
+
+@bot.message_handler(commands=['help'])
+def send_bye(message):
+    bot.reply_to(message, "чем я могу помочь?")
+
+@bot.message_handler(func=lambda message: True)
+def echo_all(message):
+    bot.reply_to(message, message.text)
+    
+bot.polling()
